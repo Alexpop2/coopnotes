5f8d7e5d7a4e1c349f61aeb423b6918ec2dbc0b1
Aleksei Poponin <>
2022-03-19 22:13:28 +0300

    Notes migration created

---
+++ database/migrations/2022_03_19_190819_create_notes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->longText('content')->nullable();
+            $table->foreignIdFor(\App\Models\User::class)
+                ->unique()
+                ->constrained('users')
+                ->onDelete('cascade');
+            $table->json('users_accessed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
